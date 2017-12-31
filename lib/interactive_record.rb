e3ceed039168f6a3666d419e49206115c02ece47
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 18:44:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,11 +56,6 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attr)
-    sql = <<-SQL
-      SELECT * FROM #{self.table_name}
-      WHERE '#{attr[0]}' = '#{attr[1]}';
-      SQL
-    DB[:conn].execute(sql)
+  
 
 end
